6bc227bb1bae33c081b3886bcea42597e4d9aa23
Swpanil <swapnil00patil@gmail.com>
2018-02-15 16:46:17 +0800

    mysql pool

--- api/utils/db.js
+++ api/utils/db.js
@@ -1,16 +1,13 @@
-
 var mysql = require('mysql');
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "jsvilla",
-    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
-});
-con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
+  
+var mysql = require('mysql');
+var connection  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : 'root',
+  database        : 'jsvilla',
+  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 });
 
-module.exports = con;
+module.exports = connection;
